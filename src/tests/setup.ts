511344654369f399847ef516e656f4dbf6501fa5
Agustin Moya <agustin.moya.dev@gmail.com>
2025-12-28 12:50:01 -0300

    test(setup): configure MSW and Faker.js for API mocking (#49)

--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -1,7 +1,25 @@
 import { afterEach } from 'vitest'
+import { beforeAll, afterAll } from 'vitest'
 
 import { cleanup } from '@testing-library/react'
 import '@testing-library/jest-dom'
 
-// Extiende las aserciones de jest-dom
-afterEach(() => cleanup())
+import { server } from './mocks/server'
+
+/**
+ * Setup MSW server for all tests.
+ * @see https://mswjs.io/docs/integrations/node
+ */
+beforeAll(() => {
+  server.listen({ onUnhandledRequest: 'warn' })
+})
+
+// Extends jest-dom assertions
+afterEach(() => {
+  cleanup()
+  server.resetHandlers()
+})
+
+afterAll(() => {
+  server.close()
+})
